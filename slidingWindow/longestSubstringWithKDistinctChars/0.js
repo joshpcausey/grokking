8f04f82ebfc1a51f1ac3377ed819ddb937eb6ae3
Joshua Causey <joshpcausey@gmail.com>
2022-03-23 13:56:00 -0500

    add comments

--- slidingWindow/longestSubstringWithKDistinctChars/0.js
+++ slidingWindow/longestSubstringWithKDistinctChars/0.js
@@ -5,20 +5,28 @@ const longestSubstringWithKDistinctChars = (input, k) => {
         charFreqCounter = {},
         maxLength = -Infinity;
 
+    // open window
     for(let windowEnd = 0; windowEnd < input.length; windowEnd++) {
         const rightChar = input[windowEnd];
 
+        // insert char into hashmap to keep track of it's frequency in the window
         if(!(rightChar in charFreqCounter)) {
             charFreqCounter[rightChar] = 0;
         }
         charFreqCounter[rightChar]++;
 
+        // our hashmap has more keys than k; begin shrinking window
         while(Object.keys(charFreqCounter).length > k) {
             const leftChar = input[windowStart];
+
+            // decrement char count in hashmap
             charFreqCounter[leftChar]--;
+
+            // if char count is 0 it's no longer in the window so delete the key
             if(charFreqCounter[leftChar] === 0) {
                 delete charFreqCounter[leftChar];
             }
+            
             windowStart++;
         }
 
